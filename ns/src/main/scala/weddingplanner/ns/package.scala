9a86f424d9e7d6e07ed68bbb5dd84f3c46a1f5c0
Thijs Broersen <mht.broersen@gmail.com>
2019-05-07 22:28:11 +0200

    added housenumber

--- ns/src/main/scala/weddingplanner/ns/package.scala
+++ ns/src/main/scala/weddingplanner/ns/package.scala
@@ -7,6 +7,7 @@ package object ns {
   val prepositionName     = Property.properties.getOrCreate("http://ns.convenantgemeenten.nl/prepositionName")
   val ssn                 = Property.properties.getOrCreate("http://ns.convenantgemeente.nl/ssn")
   val streetname          = Property.properties.getOrCreate("http://ns.convenantgemeenten.nl/streetname")
+  val houseNumber         = Property.properties.getOrCreate("http://ns.convenantgemeenten.nl/houseNumber")
   val houseLetter         = Property.properties.getOrCreate("http://ns.convenantgemeenten.nl/houseLetter")
   val underLegalRestraint = Property.properties.getOrCreate("http://ns.convenantgemeenten.nl/underLegalRestraint")
   val civilStatus         = Property.properties.getOrCreate("http://ns.convenantgemeente.nl/civilStatus")
